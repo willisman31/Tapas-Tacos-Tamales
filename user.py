673bb9c96404c90bf2c44318b2560084007e199e
Jacob Willis <willisjp@dukes.jmu.edu>
2022-06-03 09:58:41 -0400

    Creation of User Object
    Initial creation of the user object and its behaviors

---
+++ user.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Tic Tac Toe User or Player, which may be human or computer.
+Author: Willisman31
+Date: 6.3.2022
+
+Tic Tac Toe user or player with known behaviors of every user; may be extended by other classes for automation of gameplay.
+"""
+
+class User:
+
+    def User(self, symbol: char, name: str) -> none:
+        self.symbol = playSymbol
+        self.name = name
+
+    def getSymbol(self) -> char:
+        return self.symbol
+
+    def setSymbol(self, playSymbol: char) -> none:
+        self.symbol = playSymbol
+
+    def getName(self) -> str:
+        return self.name
+
+    def setName(self, newName: str) -> none:
+        self.name = newName
+
+
+
