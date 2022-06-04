926cb952d21b7d0beda0ccb45e00840c42327a99
Jacob Willis <willisjp@dukes.jmu.edu>
2022-06-04 00:12:50 -0400

    Stubbing classes and methods

---
+++ game.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Game and its logical flows
+Author: Willisman31
+Date: 6.3.2022
+
+"""
+
+class Game:
+
+    def Game(self):
+        pass
+
+    def play():
+        pass
+
+    def isWon() -> bool:
+        pass
+
---
+++ gui.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Create graphical user interface using TKinter - not needed for game to function
+Author: Willisman31
+Date: 6.3.2022
+
+"""
+
+class GUI:
+
+    pass
+
+
