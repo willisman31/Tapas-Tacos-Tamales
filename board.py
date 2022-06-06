3b3122bef8b5e2fbe6f0dd0a4b1306d6affa37a0
Jacob Willis <willisjp@dukes.jmu.edu>
2022-06-05 21:11:18 -0400

    Complete board functionality
    Complete board functionality and test

--- board.py
+++ board.py
@@ -9,17 +9,30 @@ Class to create a dynamic and extensable tic-tac-toe game board
 
 class Board:
 
-    def board(self, numberOfRows: int, numberOfColumns: int) -> none:
+    def __init__(self, numberOfRows: int, numberOfColumns: int) -> None:
         self.numberOfRows = numberOfRows
         self.numberOfColumns = numberOfColumns
         self.gameBoard = [[" "] * numberOfColumns] * numberOfRows
 
+    # Used only for Command Line, not in GUI
     def displayBoard(self) -> str:
-        pass
+        display = ""
+        for column in range(self.numberOfColumns * 2 - 1):
+            for row in range(self.numberOfRows * 2 - 1):
+                if row % 2 == 1:
+                    if column % 2 == 1:
+                        display += " "
+                    else:
+                        display += "|"
+                else:
+                    if column % 2 == 1:
+                        display += "-"
+                    else:
+                        display += " "
+            display += "\n"
+        return display
 
     def getFieldValue(self, row, column) -> str:
-        pass
+        return self.gameBoard[row][column]
 
-    def setFieldValue(self, row, column) -> none:
-        pass)
 
