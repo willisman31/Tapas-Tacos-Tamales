a032db36d800cc7b80eb792dd627de550f94517c
Jacob Willis <willisjp@dukes.jmu.edu>
2022-06-08 20:07:33 -0400

    Create constructor
    Create constructor for game class

--- game.py
+++ game.py
@@ -8,13 +8,28 @@ Date: 6.3.2022
 
 class Game:
 
-    def __init__(self) -> none:
-        board = Board
+    
+    symbolOptions = ["x", "o", "t", "l", "w", "r"]
 
-    def play() -> none:
+    def __init__(self, numberOfRows: int, numberOfColumns: int, numberOfUsers: int) -> None:
+        self.board = Board(numberOfRows, numberOfColumns)
+        self.users = self.initializeUsers(numberOfUsers, board)
+
+    def initializeUsers(self, numberOfUsers: int, board: Board) -> User[]:
+        users = []
+        for user in range(numberOfUsers):
+            symbol = symbolOptions[numberOfUsers]
+            defaultUserName = "User " + str(user + 1)
+            users.append(User(defaultUserName, board, symbol))
+        return users
+
+    def play() -> None:
         pass
 
     def isWon() -> bool:
         pass
+
+    def getNumberOfUsers(self) -> int:
+        return self.users.length
 
 
